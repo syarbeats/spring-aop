d20e6b16f0cd486b33a9b1a60dc4a4eb9aa21a2e
Syarif Hidayat <Syarif_H657@mitrais.com>
2019-02-15 13:33:27 +0700

    add pointcut example

--- src/main/java/com/learning/spring/spring_aop/EmployeeAspectPointcut.java
+++ src/main/java/com/learning/spring/spring_aop/EmployeeAspectPointcut.java
@@ -7,18 +7,20 @@ import org.aspectj.lang.annotation.Pointcut;
 @Aspect
 public class EmployeeAspectPointcut {
 
-	/*@Before("getNamePointcut()")
-	public void firstAdvice() {
-		System.out.println("[First Advice - getNamePointCut()] Executing firstAdvice on before getName executed...");
+	@Before("getNamePointcut()")
+	public void firstPointcutAdvice() {
+		System.out.println("[firstPointcutAdvice - getNamePointCut()] Executing firstPointcutAdvice before getName executed...");
 	}
 	
 	@Before("getNamePointcut()")
-	public void secondAdvice() {
-		System.out.println("[Second Advice - getNamePointCut()] Executing secondAdvice on before getName executed...");
+	public void secondPointcutAdvice() {
+		System.out.println("[secondPointcutAdvice - getNamePointCut()] Executing secondPointcutAdvice before getName executed...");
 	}
 	
-	@Pointcut("execution(public String getName())")
-	public void getNamePointcut() {};*/
+	@Pointcut("execution(String com.learning.spring.spring_aop.model.Employee.getName())")
+	public void getNamePointcut() {
+		
+	}
 	
 	
 }
